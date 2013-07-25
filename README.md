ec90ec6732c5e10e178ca758ba09764d39967353
kagemusha <mmadrid99@gmail.com>
2013-07-25 16:34:19 -0400

    first pass at updated card bootstrap

--- README.md
+++ README.md
@@ -1,30 +1,60 @@
 glazier-card-bootstrap
 ======================
 
-A starting point for a new glazier card
+This project offers a starting point for developing new glazier cards.  Note this
+is a work in progress.  In the future....  But for now, this project offers a
+relatively painless way to start developing cards.
+
+## Adding a card to glazier
+
+As you have develop a card (process described below), or want to incorporate someone
+else's card in your project, we recommend you keep the project in a separate folder
+from the glazier folder and simlink your folder into the glazier cards directory.  This
+way you can easily pull glazier project updates, while allowing you to develop and test
+your card independently.
+
+Steps for adding a card to glazier locally:
 
 
-## Adding to glazier
+    1. Simlink your card dir into the glazier cards dir
 
-    # in `glazier/cards/`
+    cd your-local-glazier/cards/
     ln -s your/path/to/glazier-card-bootstrap
+
+    2. Ingest
 
     # in `glazier/`
     grunt ingestCards
 
-    # register the card with the github repo you wish to have it available on
-    # by creating a Pane record
-    
+    3. Add your card to the github repository dashboard on which you wish to have it available
+
     # in `glazier/glazier-server/`
     bundle exec rails console
 
-    # create a Pane
+    # add the Pane to the repository dashboard of your choosing
+    # for instance to add to the yapplabs/glazier dashboard, do the followin:
+    > dashboard = Dashboard.find_or_bootstrap('yapplabs/glazier')
+
+     # if your card's name (defined ?) is 'my-awesome-card'
+    > dashboard.add_pane('my-awesome-card')
+
+    #if add_pane doesn't work, can add manually
     > pane = Pane.new
     > pane.pane_type_name="your-cards-type-name"  #refers to the name property in your card's package.json
     > pane.save!
+    > dashboard.panes.push(pane)
 
-    # add the Pane to the repository dashboard of your choosing
-    db = Dashboard.where(repository: 'yapplabs/glazier').first  #repository is the repo you wish to add card to
-    db.panes.push(pane)
+Removing a card from a dashboard
 
-In your browser, navigate to your glazier repository page.  Your card should appear there.
+    dashboard.remove_pane(card_type_name)
+
+In your browser, navigate to your repository page.  Your card should appear there.
+
+## Developing a Glazier Card
+
+  1. clone this project
+  2. rename folder and files
+  3. if using ember, copy files in ember-app-files dir to the app dir
+  4.  scope out services you need
+  5.  develop your card
+  6. ember-specific card development
--- card.js
+++ card.js
@@ -1,30 +1,46 @@
 import 'conductor' as Conductor;
 
-// If you need them, you can require additional vendored javascript files from Glazier:
+/* Uncomment/add required javascript files */
 Conductor.require('/vendor/jquery.js');
-// Conductor.require('/vendor/handlebars.js');
-// Conductor.require('/vendor/ember-latest.js');
-// Conductor.require('/vendor/loader.js');
+//Conductor.require('/vendor/handlebars.js');
+//Conductor.require('/vendor/ember-latest.js');
+//Conductor.require('/vendor/loader.js');
 
+/* Uncomment/add required css files */
+//Conductor.requireCSS('/css/glazier_card.css');
 Conductor.requireCSS('card.css');
 
 var card = Conductor.card({
+  /* uncomment or add needed services here */
   consumers: {
-    'repository': Conductor.Oasis.Consumer
+    //'fullXhr': Conductor.Oasis.Consumer,
+    //'paneUserStorage': Conductor.Oasis.Consumer,
+    //'authenticatedGithubApi': Conductor.Oasis.Consumer,
+    //'unauthenticatedGithubApi': Conductor.Oasis.Consumer,
+    'repository': Conductor.Oasis.Consumer,
+    'test': TestConsumer
+
   },
 
   render: function (intent, dimensions) {
-    if (!dimensions) {
-      dimensions = {
-        width: 500,
-        height: 300
-      };
-    }
 
     document.body.innerHTML = "<div id=\"card\">Hello Bootstrap.  Click me.</div>";
+    /*
+      After verifying card bootstrapped, replace above line with line below
+      and remove (or overwrite) the defaultOnActivation function and its call.
+
+      If using Ember, the App.rootElement is set to '#card', so you will need
+      it in your DOM as below.
+     */
+    //document.body.innerHTML = "<div id=\"card\"></div>";
+
+
+    /* Remove this line if not using Ember.js in your card */
+    //todo make this a try/catch with appropriate msg if can't find ember
+    Ember.run(this.App, 'advanceReadiness');
   },
 
-  activate: function() {
+  defaultOnActivation: function(){
     card.consumers.repository.request('getRepository').then(function(name) {
       $('#card').click(function() {
         alert('You clicked me in ' + name);
@@ -32,14 +48,19 @@ var card = Conductor.card({
     });
   },
 
-  resize: function(dimensions) {
-    var width = Math.min(dimensions.width, 500);
-    var height = Math.min(dimensions.height, 500);
+  activate: function() {
+    this.App = requireModule('app/application');
+    this.defaultOnActivation();
+  },
 
-    $('body>div').css({
-      width: width
-    });
+  metadata: {
+    document: function(promise) {
+      promise.resolve({
+        title: "Give this card a proper title in card.js card.metadata"
+      });
+    }
   }
+
 });
 
 export = card;
